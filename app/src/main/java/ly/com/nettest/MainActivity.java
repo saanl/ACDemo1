37aa0e27346d51d96973f836a3de7da34dd1fcc7
Will Liu <willl@wicresoft.com>
2019-07-11 11:08:28 +0800

    update

--- app/src/main/java/ly/com/nettest/MainActivity.java
+++ app/src/main/java/ly/com/nettest/MainActivity.java
@@ -5,14 +5,22 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCenter.start(getApplication(), "34d407a4-d76a-4792-8254-46a24a2a9a8c",
+                Analytics.class, Crashes.class);
+
         WebView webView = findViewById(R.id.web);
-        webView.loadUrl("http://www.baidu.com");
+        webView.loadUrl("https://www.baidu.com");
         WebSettings webSettings=webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
     }
