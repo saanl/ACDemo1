eb4850f3b7fb27290ab5406d00a4e1ef7329e140
Will Liu <willl@wicresoft.com>
2019-07-11 11:30:46 +0800

    update

--- app/src/main/java/ly/com/nettest/MainActivity.java
+++ app/src/main/java/ly/com/nettest/MainActivity.java
@@ -2,12 +2,16 @@ package ly.com.nettest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.push.Push;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +21,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AppCenter.start(getApplication(), "34d407a4-d76a-4792-8254-46a24a2a9a8c",
-                Analytics.class, Crashes.class);
+                Analytics.class, Crashes.class,Push.class);
 
         WebView webView = findViewById(R.id.web);
         webView.loadUrl("https://www.baidu.com");
         WebSettings webSettings=webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+    }
+
+    public void click1(View view){
+
+        Random random = new Random();
+        int nextInt = random.nextInt(15);
+
+        if(nextInt % 2 == 0){
+            int k = 9/0;
+        }else if(nextInt % 3 == 0){
+            Integer s = null;
+            s.equals(89);
+        }else if(nextInt %5 == 0){
+            String[] s = {"",""};
+            String s1 = s[3];
+        }else{
+            throw new RuntimeException("测试Crash");
+        }
+
     }
 }
